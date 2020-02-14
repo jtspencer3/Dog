e44260d9e4a42d70525489b6facfc7038371ce9c
Joel Spencer <joel@Joels-MacBook-Pro.local>
2020-02-14 17:25:27 -0600

    added GetTag and fixed enum

--- Dog.cs
+++ Dog.cs
@@ -2,9 +2,9 @@
 
 namespace Dog
 {
+    public enum Gender {Male, Female}
     public class Dog
     {
-        public enum Gender {Male, Female}
           public Dog(string dogName, string ownerName, int dogAge, Gender doggender)
         {
             name = dogName;
@@ -26,6 +26,29 @@ namespace Dog
                 Console.WriteLine("woof!");
             }
          }
+
+          public string GetTag()
+        {
+            string tag = "";
+            if(gender == Gender.Male && age >1)
+            {
+                tag = "if lost, call " + owner + ". His name is " + name + " and he is " + age + "years old.";
+
+            }else if(gender == Gender.Male && age <= 1)
+            {
+                tag = "If lost, call " + owner + ". His name is " + name + " and he is " + age + "year old.";
+            
+            }else if(gender == Gender.Female && age>1)
+            {
+                tag = "If lost, call " + owner + ". Her name is " + name + " and he is " + age + "years old.";
+
+            }else if(gender == Gender.Female && age<=1)
+            {
+                tag = "If lost, call " + owner + ". Her name is " + name + " and she is" + age + "year old.";
+
+            }
+            return tag;
+        }
     }
     class Program     
     {
@@ -37,7 +60,7 @@ namespace Dog
             
             Dog max = new Dog("max", "kyle", 3, Gender.Female);
             max.Bark(2);
+            Console.WriteLine(max.GetTag());
         }
     }
-
 }

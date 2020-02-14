421889d5176fd1150e5ec3070f6a12f9274e2823
Joel Spencer <joel@Joels-MacBook-Pro.local>
2020-02-14 14:37:39 -0600

    first commit

---
+++ Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Dog
+    {
+        
+    }
+}

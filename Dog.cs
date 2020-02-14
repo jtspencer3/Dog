7769def930a9bcc603f75b41087c7cc2be079e5d
Joel Spencer <joel@Joels-MacBook-Pro.local>
2020-02-14 16:47:41 -0600

    trying to fix the name gender does not exist

--- Dog.cs
+++ Dog.cs
@@ -2,16 +2,42 @@
 
 namespace Dog
 {
-    class Program
+    public class Dog
+    {
+        public enum Gender {Male, Female}
+          public Dog(string dogName, string ownerName, int dogAge, Gender doggender)
+        {
+            name = dogName;
+            owner = ownerName;
+            age = dogAge;
+            gender = doggender;
+        }
+        public string name;
+        public string owner;
+        public int age;
+ 
+        public Gender gender;
+        
+        public void Bark(int barkamount)
+        {
+   
+            for(int i = 0;i<barkamount;i++)
+            {
+                Console.WriteLine("woof!");
+            }
+         }
+    }
+    class Program     
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dog dog = new Dog("geoff","greg", 2, Gender.Male);
+            dog.Bark(3);
+            Console.WriteLine(dog.GetTag());
+            
+            Dog max = new Dog("max", "kyle", 3, Gender.Female);
+            max.Bark(2);
         }
     }
 
-    public class Dog
-    {
-        
-    }
 }
